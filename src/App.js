350a991f3e77df2c135921fda97e55210f322c08
michealsource <kelvinmicheal209@gmail.com>
2022-03-23 10:25:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import React,{useState,useEffect} from 'react'
+import IndexRoutes from './Routes/index'
+// import { requestFirebaseNotificationPermission, onMessageListener } from "./firebaseConfig"
+import {ShowMessage, type} from "../src/Component/Toaster"
+import LandingPage from './Pages/landingPage.js/LandingPage'
+function App() {
+//   const [token, setToken] = useState("")
+//   requestFirebaseNotificationPermission()
+//   .then((firebaseToken) => {
+//     // eslint-disable-next-line no-console
+//     // console.log(firebaseToken);
+//     localStorage.setItem("firebaseToken", firebaseToken)
+//     setToken(firebaseToken)
+//   })
+//   .catch((err) => {
+//     return err;
+//   });
+
+// onMessageListener()
+// .then((payload) => {
+//   const { title, body } = payload.data;
+//   ShowMessage(`${title}; ${body}`)
+// })
+// .catch((err) => {
+//   ShowMessage(type.ERROR, JSON.stringify(err))
+// });
+
+  return (
+    // <IndexRoutes/>
+    <LandingPage/>
+  );
+}
+
+export default App;
